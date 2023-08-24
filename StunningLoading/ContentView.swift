7419bf408728ca77fedc5d92365a468f54627268
Elaidzha1940 <eli080293@gmail.com>
2023-08-24 16:54:52 +0300

    Stunning Loading Animations with SwiftUI

---
+++ StunningLoading/ContentView.swift
@@ -0,0 +1,27 @@
+//  /*
+//
+//  Project: StunningLoading
+//  File: ContentView.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 24.08.2023
+//
+//  Status: in progress | Decorated
+//
+//  */
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        
+        VStack {
+            
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ StunningLoading/StunningLoadingApp.swift
@@ -0,0 +1,21 @@
+//  /*
+//
+//  Project: StunningLoading
+//  File: StunningLoadingApp.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 24.08.2023
+//
+//  Status: in progress | Decorated
+//
+//  */
+
+import SwiftUI
+
+@main
+struct StunningLoadingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
