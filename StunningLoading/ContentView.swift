f09329de8ae0d6c77fd0a46059a1cf4e87700471
Elaidzha1940 <eli080293@gmail.com>
2023-08-24 17:11:31 +0300

    StunningLoading

--- StunningLoading/ContentView.swift
+++ StunningLoading/ContentView.swift
@@ -30,49 +30,53 @@ struct ContentView: View {
                 .frame(width: 50, height: 50)
                 .rotation3DEffect(.degrees(75), axis: (x: 1, y: 0, z: 0))
                 .offset(y: jumping ? 150 : -180)
-                .animation(.interpolatingSpring(stiffness: 100, damping: 5).repeatForever(autoreverses: true), value: jumping)
+                .animation(.interpolatingSpring(stiffness: 100, damping: 5).repeatForever(autoreverses: true).delay(0.05), value: jumping)
             
             Circle()
                 .stroke(lineWidth: 5)
                 .frame(width: 80, height: 80)
                 .rotation3DEffect(.degrees(75), axis: (x: 1, y: 0, z: 0))
                 .offset(y: jumping ? 150 : -180)
-                .animation(.interpolatingSpring(stiffness: 100, damping: 5).repeatForever(autoreverses: true), value: jumping)
+                .animation(.interpolatingSpring(stiffness: 100, damping: 5).repeatForever(autoreverses: true).delay(0.01), value: jumping)
             
             Circle()
                 .stroke(lineWidth: 5)
                 .frame(width: 110, height: 110)
                 .rotation3DEffect(.degrees(75), axis: (x: 1, y: 0, z: 0))
                 .offset(y: jumping ? 150 : -180)
-                .animation(.interpolatingSpring(stiffness: 100, damping: 5).repeatForever(autoreverses: true), value: jumping)
+                .animation(.interpolatingSpring(stiffness: 100, damping: 5).repeatForever(autoreverses: true).delay(0.15), value: jumping)
             
             Circle()
                 .stroke(lineWidth: 5)
                 .frame(width: 140, height: 140)
                 .rotation3DEffect(.degrees(75), axis: (x: 1, y: 0, z: 0))
                 .offset(y: jumping ? 150 : -180)
-                .animation(.interpolatingSpring(stiffness: 100, damping: 5).repeatForever(autoreverses: true), value: jumping)
+                .animation(.interpolatingSpring(stiffness: 100, damping: 5).repeatForever(autoreverses: true).delay(0.2), value: jumping)
             
             Circle()
                 .stroke(lineWidth: 5)
                 .frame(width: 170, height: 170)
                 .rotation3DEffect(.degrees(75), axis: (x: 1, y: 0, z: 0))
                 .offset(y: jumping ? 150 : -180)
-                .animation(.interpolatingSpring(stiffness: 100, damping: 5).repeatForever(autoreverses: true), value: jumping)
+                .animation(.interpolatingSpring(stiffness: 100, damping: 5).repeatForever(autoreverses: true).delay(0.25), value: jumping)
             
             Circle()
                 .stroke(lineWidth: 5)
                 .frame(width: 200, height: 200)
                 .rotation3DEffect(.degrees(75), axis: (x: 1, y: 0, z: 0))
                 .offset(y: jumping ? 150 : -180)
-                .animation(.interpolatingSpring(stiffness: 100, damping: 5).repeatForever(autoreverses: true), value: jumping)
+                .animation(.interpolatingSpring(stiffness: 100, damping: 5).repeatForever(autoreverses: true).delay(0.3), value: jumping)
             
             Circle()
                 .stroke(lineWidth: 5)
                 .frame(width: 230, height: 230)
                 .rotation3DEffect(.degrees(75), axis: (x: 1, y: 0, z: 0))
                 .offset(y: jumping ? 150 : -180)
-                .animation(.interpolatingSpring(stiffness: 100, damping: 5).repeatForever(autoreverses: true), value: jumping)
+                .animation(.interpolatingSpring(stiffness: 100, damping: 5).repeatForever(autoreverses: true).delay(0.35), value: jumping)
+        }
+        .onAppear {
+            
+            jumping.toggle()
         }
     }
 }
